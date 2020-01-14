4726d5fcc2e4f2e9e7d24ee109419322bb3af8f1
HollyMaguire <Hollygracemaguire@gmail.com>
2020-01-14 14:10:53 -0600

    first commit

---
+++ src/views/countingCharacters.vue
@@ -0,0 +1,35 @@
+<template>
+<v-app  id="countingApp">
+  <div>
+      Enter your word:<v-text-field v-model="word">rtj
+      </v-text-field>
+      </div>
+      <div>
+          The length of your word is {{length}} 
+          
+    </div>
+</v-app>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            word: ""
+        }
+    },
+    computed: {
+        length() {
+            return (this.word.length);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+#countingApp {background-color:rgb(121, 241, 235)
+}
+</style>
---
+++ src/views/profile.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app id="mainPage">
+     <v-container>
+                  <p>
+                      <p class="h1"><strong>About Me <a href="https://github.com/HollyMaguire"> Link to My Git Hub</a></strong ></p> 
+                              <p> I created this site with links to some of my projects to show what I can do so far. This was made in Vue, I have Python projects on my Github as well as a HTML formatted portfolio, which contains the same projects in HTML/CSS alone.  
+I am trying  to get into the computer science field, although my degree is in chemistry. </p>
+                              <p>I went to Prairie view A&M to obtain my bachelor’s degree in chemistry. During my senior research project, I found an enjoyment for the machines in the lab. I learned to use various machines and software for each. I then went to Texas A&M to work doing research, it was in this time that I first dabbled with programing. As I mentioned before I took a liking the working with the machines, but now there were robots to work with. I learned how to use and program the robots. One of these robots I frequently used had its own, but simple programing language, this is where I noticed an enjoyment for coding. I have been learning to code by doing classes on Udemy, I am currently doing a java class. I know JavaScript, Python, HTML, CSS and Vue
+                           </p>
+    
+              <p class="h1"><strong>Contact Me</strong> </p>            
+               <p>Phone:832-620-0172</p>   
+            <p>Email:hollygracemaguire@gmail.com</p> 
+           
+              
+                  <p class="h1"><strong> Experience</strong></p> <br/>
+                              <p>Although my past experience has been very different from the route that I am now pursuing, there are still valuable skills that translate over. I have a lot of analytical skills from my time in research, it also honed skills for problem solving and critical thinking. It was also in this time that I got my first introduction to programming, I would program the robots. </p>
+                              <p>I was the project lead for a Malaria drug discovery project. I gained experience working in teams as well from the Menkes syndrome project, cryptosporidium drug study, QT prolongations studies, and cancer research. All animal work was done closely with a team, the previously mentioned projects involved the use of animals. I also have experience working independently from a cloning and gene-splicing project, and from my time doing polymer research. I gained experience, and found that am comfortable working in all types of roles, from leadership, to simply following protocols.</p>
+                       
+         
+          
+                  <p class="h1"><strong> Resume</strong></p> <br/>
+                              
+              <v-row>   
+                           <v-img src='../../src/assets/resume.jpg' contain
+    height="900px"
+    width="850px"></v-img>
+              </v-row>
+              
+      </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#mainPage{ background-color: lightblue}
+#v-row {
+    background-color: black;
+}
+v-app {
+    background-color: lightblue;
+  font-family: arial, helvetica, sans-serif;
+    color: #0d1f2d;
+    margin: 0 auto;
+}
+
+header, footer {
+    background-color: indianred;
+    color: white;
+}
+
+
+
+caption {
+    font-weight: bold;
+    font-size: 1.2em;
+    margin-bottom: 8px;
+}
+
+
+header, footer {
+    background-color: #546a7b;
+    color:black;
+    height: 100px;
+    text-align: right;
+    padding: 30px 10px 20px 10px;
+}
+
+.h1 {font-size: 1.5em;
+text-decoration-color: black;
+text-align: Center}
+
+
+v-container {width: 100%}
+</style>
