2cb8f26f6f907feebf03f4616deb1f01344a2e34
HollyMaguire <Hollygracemaguire@gmail.com>
2021-05-21 13:28:20 -0500

    change default component

--- src/router.js
+++ src/router.js
@@ -70,6 +70,11 @@ export default new Router({
       component: Memory 
     },
     {
+      path: '/',
+      name: 'Memory',
+      component: Memory 
+    },
+    {
       path: '/dice',
       name: 'dice',
       component: dice
